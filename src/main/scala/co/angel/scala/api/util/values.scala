abc1f03d0a646315017ef89e92bb40c0d424e678
aldonline <aldo.bucchi@gmail.com>
2013-10-31 03:15:16 -0700

    create some case classes and corresponding implicit conversions to represent  common values

---
+++ src/main/scala/co/angel/scala/api/util/values.scala
@@ -0,0 +1,25 @@
+package co.angel.scala.api.util
+
+object values {
+
+  trait ALStringValue {
+    def str:String
+    override def toString = str
+  }
+  
+  case class ALId( str:String ) extends ALStringValue
+  case class ALSlug( str:String ) extends ALStringValue
+  case class ALEmail( str:String ) extends ALStringValue
+  case class ALMD5Hash( str:String ) extends ALStringValue
+  case class ALBearerToken( str:String ) extends ALStringValue 
+  
+  
+  implicit def angellistvalue2str( v:ALStringValue ):String = v.str 
+  
+  implicit def toALBearerToken( str:String ) = ALBearerToken( str )
+  implicit def toALId( str:String ) = ALId( str )
+  implicit def toALSlug( str:String ) = ALSlug( str )
+  implicit def toALMD5Hash( str:String ) = ALMD5Hash( str )
+  implicit def toALALEmail( str:String ) = ALEmail( str )
+  
+}
